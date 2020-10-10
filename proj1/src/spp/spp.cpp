dd3ba38842aa5de2f05fb96c3bb3c7bd3d794ff6
john <799433746@qq.com>
2020-10-10 23:38:03 +0800

    use g++

---
+++ proj1/src/spp/spp.cpp
@@ -0,0 +1,5 @@
+//
+// Created by john on 10/10/20.
+//
+
+#include "spp.h"
---
+++ proj1/src/spp/spp.h
@@ -0,0 +1,8 @@
+//
+// Created by john on 10/10/20.
+//
+
+#ifndef PROJ1_SPP_H
+#define PROJ1_SPP_H
+
+#endif //PROJ1_SPP_H
